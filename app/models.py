feda4cd21484b889e32cb88d15a4dadbed0fb7e8
shockwave07 <varunraskar22@gmail.com>
2024-02-09 15:54:18 +0530

    4th chapter completed

--- app/models.py
+++ app/models.py
@@ -2,16 +2,30 @@ from typing import Optional
 from datetime import datetime, timezone
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db,login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(UserMixin,db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    user: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index =True, unique=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                             unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -25,3 +39,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+    
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
